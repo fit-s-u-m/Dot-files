ab5186487f264e0d01dbf76ce3b726ea1430f336
fit-s-u-m <nehemiahmaths@gmail.com>
2024-08-22 02:33:02 +0300

    added some plugin to nvim

---
+++ .config/nvim/lua/custom/plugins/zen-mode.lua
@@ -0,0 +1,19 @@
+-- Lua
+return {
+	"folke/zen-mode.nvim",
+	opts = {
+		window = {
+			width = 1,
+			height = 1,
+			options = {
+				signcolumn = "no", -- disable signcolumn
+				number = false, -- disable number column
+				relativenumber = false, -- disable relative numbers
+				cursorline = false, -- disable cursorline
+				cursorcolumn = false, -- disable cursor column
+				foldcolumn = "0", -- disable fold column
+				list = false, -- disable whitespace characters
+			},
+		},
+	},
+}
